3151c425afc305476bca9408489fae37383d472e
Johann Höchtl <johann.hoechtl@gmail.com>
2016-05-16 17:23:15 +0200

    CSV header similarity web service

--- cmd/csvsimservice/main.go
+++ cmd/csvsimservice/main.go
@@ -1,23 +1,102 @@
-// Copyright (c) Johann Höchtl 2016
+package main // Copyright (c) Johann Höchtl 2016
 //
 // See LICENSE for License
 
-// Beginnings of a test to compare the similarity between two CSV-headers
-package main
+// RESTful service to check for the similarity of CSV headers
 
-import "github.com/the42/setsim"
+import (
+	"encoding/csv"
+	"fmt"
+	"log"
+	"net/http"
+	"os"
+	"strings"
+
+	"github.com/emicklei/go-restful"
+	"github.com/the42/setsim"
+)
+
+// CSVPairCompareRequest document
+type CSVPairCompareRequest struct {
+	Comma    *string
+	Comment  *string
+	Baseline *string
+	Compare  *string
+}
+
+// CSVPairCompareResponse document
+type CSVPairCompareResponse struct {
+	CSVPairCompareRequest
+	CompareResult []int `json:"compareResult"`
+}
+
+func compareCSVHeader(request *restful.Request, response *restful.Response) {
+	csvset := CSVPairCompareRequest{}
+	if err := request.ReadEntity(&csvset); err != nil {
+		logresponse(response, http.StatusBadRequest, fmt.Sprintf("unable to parse request: %s", err.Error()))
+		return
+	}
+
+	if csvset.Baseline == nil || csvset.Compare == nil {
+		logresponse(response, http.StatusBadRequest, "insufficient data to compare")
+		return
+	}
+
+	basereader := csv.NewReader(strings.NewReader(*csvset.Baseline))
+	comparereader := csv.NewReader(strings.NewReader(*csvset.Compare))
+
+	if comma := csvset.Comma; comma != nil {
+		basereader.Comma = rune((*comma)[0])
+		comparereader.Comma = basereader.Comma
+	}
+	if comment := csvset.Comment; comment != nil {
+		basereader.Comment = rune((*comment)[0])
+		comparereader.Comment = basereader.Comment
+	}
+
+	baserecords, err := basereader.ReadAll()
+	if err != nil {
+		logresponse(response, http.StatusInternalServerError, err.Error())
+		return
+	}
+	if len(baserecords) == 0 {
+		logresponse(response, http.StatusInternalServerError, err.Error())
+		return
+	}
+
+	comparerecords, err := comparereader.ReadAll()
+	if err != nil {
+		log.Fatal(err)
+	}
+	if len(comparerecords) == 0 {
+		logresponse(response, http.StatusInternalServerError, "no baseline header")
+		return
+	}
+
+	result := CSVPairCompareResponse{CSVPairCompareRequest: csvset}
+	for _, val := range comparerecords {
+		result.CompareResult = append(result.CompareResult, setsim.StringDistance(baserecords[0], val))
+	}
+	response.WriteAsJson(result)
+}
+
+func logresponse(resp *restful.Response, code int, message string) {
+	resp.WriteErrorString(code, message)
+	log.Print(message)
+}
 
 func main() {
-	var doc1 = []string{
-		"Date", "Time", "ZIP", "Address", "Reason",
-	}
-	var doc2 = []string{
-		"Time", "Date", "ZIP", "Address", "Reason",
-	}
-	var doc3 = []string{
-		"Date1", "Time", "ZIP", "Address", "Reason",
+	ws := new(restful.WebService).
+		Produces(restful.MIME_JSON).
+		Consumes(restful.MIME_JSON)
+
+	ws.Route(ws.PUT("/compare").To(compareCSVHeader))
+	restful.Add(ws)
+
+	port := os.Getenv("PORT")
+	if port == "" {
+		port = "5000"
 	}
 
-	println(setsim.StringDistance(doc1, doc2))
-	println(setsim.StringDistance(doc1, doc3))
+	log.Fatal(http.ListenAndServe(":"+port, nil))
 }
